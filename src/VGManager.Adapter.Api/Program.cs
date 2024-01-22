e06ddfc0acd6dd240ca2e3fc9faef24e86fd5b96
Viktor Bocz-Kemeny <beviktor95@gmail.com>
2024-01-22 16:54:59 +0100

    Fixed program.cs

--- src/VGManager.Adapter.Api/Program.cs
+++ src/VGManager.Adapter.Api/Program.cs
@@ -1,25 +1,15 @@
+using VGManager.Adapter.Api;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var specificOrigins = Constants.Cors.AllowSpecificOrigins;
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Logging.ClearProviders();
+builder.Logging.AddSimpleConsole();
+
+ConfigureServices(builder, specificOrigins);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+await ConfigureAsync(app, specificOrigins);
+await app.RunAsync();
