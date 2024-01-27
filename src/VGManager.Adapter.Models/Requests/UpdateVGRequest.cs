cf9040a03f2f0ff0980fa05989135f8c2125898b
Viktor Bocz-Kemeny <beviktor95@gmail.com>
2024-01-27 13:49:11 +0100

    Renamed request model to have a more meaningful name

--- src/VGManager.Adapter.Models/Requests/UpdateVGRequest.cs
+++ src/VGManager.Adapter.Models/Requests/UpdateVGRequest.cs
@@ -1,10 +1,8 @@
-
 using Microsoft.TeamFoundation.DistributedTask.WebApi;
-using VGManager.Adapter.Azure.Services.Requests;
 
 namespace VGManager.Adapter.Models.Requests;
 
-public class VGRequest: ExtendedBaseRequest
+public class UpdateVGRequest : ExtendedBaseRequest
 {
     public VariableGroupParameters Params { get; set; } = null!;
     public int VariableGroupId { get; set; }
