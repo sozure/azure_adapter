f45b352f5ab6c3aa35d1146ed5e3f97da8dc5bb6
Viktor Bocz-Kemeny <beviktor95@gmail.com>
2024-01-28 16:41:53 +0100

    Added new properties for potential needs

--- src/VGManager.Adapter.Models/Requests/GetVGRequest.cs
+++ src/VGManager.Adapter.Models/Requests/GetVGRequest.cs
@@ -2,7 +2,10 @@ namespace VGManager.Adapter.Models.Requests;
 
 public class GetVGRequest: ExtendedBaseRequest
 {
+    public bool? KeyIsRegex { get; set; }
+    public string KeyFilter { get; set; } = null!;
     public bool ContainsSecrets { get; set; }
     public bool FilterAsRegex { get; set; }
     public string VariableGroupFilter { get; set; } = null!;
+    public string[]? PotentialVariableGroups { get; set; } = null!;
 }
